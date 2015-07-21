2711602341ebb517bb5dc762047da4500a6e64c3
Tomas Kvietkauskas <tomas@kvietkauskas.lt>
2015-07-21 11:41:40 +0300

    file cache used nfq http client

--- app/src/main/java/com/nfq/hh/icekiosk/app/FileCache.java
+++ app/src/main/java/com/nfq/hh/icekiosk/app/FileCache.java
@@ -3,23 +3,40 @@ package com.nfq.hh.icekiosk.app;
 import android.content.Context;
 import android.util.Log;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class FileCache {
 
     private File cacheDir;
+    private Context context;
 
     public FileCache(Context context){
+
+        this.context = context;
+
         //Find the dir to save cached images
         if (android.os.Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED)) {
             cacheDir = new File(android.os.Environment.getExternalStorageDirectory(), "IceKiosk");
         }
         else {
-            cacheDir = context.getCacheDir();
+            cacheDir = this.context.getCacheDir();
         }
         if (!cacheDir.exists()) {
             cacheDir.mkdirs();
@@ -34,8 +51,20 @@ public class FileCache {
         file = new File(cacheDir, filename);
 
         if (!file.isFile()) {
+
+            HttpClient httpclient = new NfqHttpClient(context);
+            HttpGet httpGet = new HttpGet(url);
+            HttpResponse response;
+
             try {
-                InputStream is = new URL(url).openStream();
+                // http way
+                // InputStream is = new URL(url).openStream();
+
+                // people ssl way
+                response = httpclient.execute(httpGet);
+                HttpEntity entity = response.getEntity();
+                InputStream is = entity.getContent();
+
                 OutputStream os = new FileOutputStream(file);
                 copyStream(is, os);
                 os.close();
