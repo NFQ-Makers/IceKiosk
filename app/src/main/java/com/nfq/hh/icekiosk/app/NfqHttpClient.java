0c00b4b7f5b19b656bb6e4f310b49882df0a8cf2
Tomas Kvietkauskas <tomas@kvietkauskas.lt>
2015-07-21 11:40:16 +0300

    nfq http client with *.nfq.lt bks

---
+++ app/src/main/java/com/nfq/hh/icekiosk/app/NfqHttpClient.java
@@ -0,0 +1,49 @@
+/**
+ * http://blog.crazybob.org/2010/02/android-trusting-ssl-certificates.html
+ */
+
+package com.nfq.hh.icekiosk.app;
+
+import android.content.Context;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.SingleClientConnManager;
+
+import java.io.InputStream;
+import java.security.KeyStore;
+
+public class NfqHttpClient extends DefaultHttpClient {
+
+    final Context context;
+
+    public NfqHttpClient(Context context) {
+        this.context = context;
+    }
+
+    @Override protected ClientConnectionManager createClientConnectionManager() {
+        SchemeRegistry registry = new SchemeRegistry();
+        registry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
+        registry.register(new Scheme("https", newSslSocketFactory(), 443));
+
+        return new SingleClientConnManager(getParams(), registry);
+    }
+
+    private SSLSocketFactory newSslSocketFactory() {
+        try {
+            KeyStore trusted = KeyStore.getInstance("BKS");
+            InputStream in = context.getResources().openRawResource(R.raw.people_nfq_lt);
+            try {
+                trusted.load(in, "123456".toCharArray());
+            } finally {
+                in.close();
+            }
+            return new SSLSocketFactory(trusted);
+        } catch (Exception e) {
+            throw new AssertionError(e);
+        }
+    }
+}
